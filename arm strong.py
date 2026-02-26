7d26f3168f2b58e5ef11eee3d74b1dd1ec08c11f
shivi5463 <shivaansh.jain27@gmail.com>
2026-02-26 16:59:36 +0530

    Add files via upload

---
+++ arm strong.py
@@ -0,0 +1,23 @@
+# Program to find if a number is Armstrong number
+
+# Take input from the user
+number = int(input("Enter your number: "))
+
+# Calculate number of digits
+digits = len(str(number))
+
+# Initialize result variable
+resultNumber = 0
+
+# find the sum of the ^digits of each digit
+temp = number
+while temp > 0:
+    digit = temp % 10
+    resultNumber += digit ** digits
+    temp //= 10
+
+# display the result
+if number == resultNumber:
+    print(number, "is an Armstrong number")
+else:
+    print(number, "is not an Armstrong number")
